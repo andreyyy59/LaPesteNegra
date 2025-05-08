5975de95bfde50ee887f1e0c486eddb356df060f
Andrey David Morales Diaz <amorales96@unab.edu.co>
2025-05-08 10:01:27 -0500

    16

--- Assets/Scripts/Menu/MenuManager.cs
+++ Assets/Scripts/Menu/MenuManager.cs
@@ -7,7 +7,14 @@ public class MainMenu : MonoBehaviour
     {
         SceneManager.LoadScene("Celdas"); // Cambia por el nombre real de tu escena de juego
     }
-
+    public void Options()
+    {
+        SceneManager.LoadScene("Opciones"); // Cambia por el nombre real de tu escena de juego
+    }
+    public void Volver()
+    {
+        SceneManager.LoadScene("Menu"); // Cambia por el nombre real de tu escena de juego
+    }
     public void QuitGame()
     {
         Application.Quit();
